318e8e145a669c5758746f2230315e652bd0b8b8
Pratik Mahobiya <pratikmahobiya18@gmail.com>
2022-12-27 10:27:25 +0530

    Host Allowed

--- tastebest/settings.py
+++ tastebest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--!7x-dj%dm093c*ja*puedp4p0z^w_mr(@5c-73kiluhc1g)z#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
